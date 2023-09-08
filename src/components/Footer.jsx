156da9ee2e8114cf48be24e442fdb420d60c5d68
Utkarsh Gupta <111635674+Geekyutkarsh23@users.noreply.github.com>
2023-09-09 02:42:34 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Made By Utkarsh</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="linkedin.com/in/utkarsh-gupta-53411b200" target={"blank"}>
+            LinkedIn
+          </a>
+          <a href="/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Geekyutkarsh23" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
